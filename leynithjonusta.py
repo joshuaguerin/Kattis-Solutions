bd677d9eae32836c48e8564b48c1e4f7ba376de8
Joshua T. Guerin <jtguerin@gmail.com>
2026-08-21 09:36:16 -0600

    Name change.

--- leynithjonusta.py
+++ leynithjonusta.py
@@ -1,3 +1,5 @@
 # Easy, strings, filter, join, lambda
+# https://open.kattis.com/problems/leynithjonusta
+# Link included to avoid thorn in file name.
 
 print(''.join(list(filter(lambda x: not str.isspace(x), input()))))
